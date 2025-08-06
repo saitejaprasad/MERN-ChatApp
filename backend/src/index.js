c99cea98e4b517794cd30c3f2e63d67b6a0e754e
saitejaprasad <saitejasunkara8@gmail.com>
2025-08-06 16:24:36 -0500

    pathset

--- backend/src/index.js
+++ backend/src/index.js
@@ -44,9 +44,10 @@ app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("/:pathMatch(.*)*", (req, res) =>
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
-  );
+
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
 }
 
 // if (process.env.NODE_ENV === "production") {
